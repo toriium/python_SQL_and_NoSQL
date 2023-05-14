e35fc3eca0be40deeeedfc3d344c3cd32d2233bd
jorgepvasconcelos <jorge.r.p.v@hotmail.com>
2023-05-13 21:40:32 -0300

    feat: add search_data

--- ex_elasticsearch/main.py
+++ ex_elasticsearch/main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from pprint import pprint
 from elasticsearch import Elasticsearch
 
 
@@ -7,15 +9,43 @@ def get_elastic() -> Elasticsearch:
     return Elasticsearch(host)
 
 
+def get_data_by_id():
+    es = get_elastic()
+
+    response = es.get(index='index_test', id=2)
+    pprint(response['_source'])
+
+
+def search_data():
+    es = get_elastic()
+
+    nome = 'jorge'
+
+    body = {
+        "query": {
+            "match": {
+                "nome": ""+nome+""
+            }
+
+        }}
+
+    response = es.search(index='index_test', body=body)
+    pprint(response)
+
+    for v in response['hits']['hits']:
+        print(v['_source'])
+
+
 def insert_data():
     es = get_elastic()
 
     body = {
         "nome": 'jorge',
-        "idade": 44, }
+        "idade": 44,
+        "timestamp": datetime.now()}
     response = es.index(index='index_test', id=2, body=body)
-    print(response['result'])
+    pprint(response['result'])
 
 
 if __name__ == '__main__':
-    insert_data()
+    search_data()
